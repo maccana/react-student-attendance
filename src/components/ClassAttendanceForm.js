82a8bf2ae7044d206ae559230224d3bf3d874686
IT-ACA <mikeymccann@gmail.com>
2023-05-24 13:33:05 -0400

    Cleaned up and created first component

---
+++ src/components/ClassAttendanceForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ClassAttendanceForm = () => {
+    return (
+        <>
+            <div style={{ display: 'flex', flexDirection: 'column', width: '80%', margin: 'auto' }}>
+
+                <h2 style={{ textAlign: 'center' }}> Attendance Form </h2>
+                <ul>
+                    <li>Create a list students</li>
+                    <li>Add radio buttons to select attendance status</li>
+                    <li>Create submit buttom to send attendance</li>
+                </ul>
+            </div>
+
+        </>
+    )
+}
+
+
+export default ClassAttendanceForm;
